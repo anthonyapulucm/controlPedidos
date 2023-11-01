97220993ba2b39d5a621f4ac2f20bd00169ffece
anthonyapulucm <145887189+anthonyapulucm@users.noreply.github.com>
2023-11-01 12:50:57 -0600

    ACTUALIZACION

---
+++ controller.php
@@ -0,0 +1,17 @@
+<?php
+if (¡empty($_POST["btnIniciar"])){
+    if (empty($_POST["usuario"]) and empty($_POST["password"])) {
+        echo '<div class ="alert alert-danger">LOS CAMPOS ESTAN VACIOS</div>';
+    } else {
+        $usuario = $_POST["usuario"];
+        $password = $_POST["password"];
+        $sql = $conexion -> query("SELECT * FROM USUARIO WHERE usuario = '$usuario' and PASSWORD = '$password'");
+        if ($datos = $sql->fetch_object()) {
+            header("location: index.php");
+        } else {
+            echo '<div class ="alert alert-danger">USUARIO Y CONTRASEÑA INCORRECTOS</div>';  
+        }
+        
+    }  
+}
+?>
---
+++ login.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CONTROL DE PEDIDOS</title>
+  <style>
+  </style>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+
+<body background="blue-sky">
+<?php
+include("conexion.php");
+include("controller.php");
+?>
+  <div class="" class="container-fluid">
+    <img src="../image/education.jpg" class="img-fluid" alt="">
+    <center>
+      <h1 style="color:black">SISTEMA DE PEDIDOS 2024</h1>
+      <a href="<?= base_url('verRegiones') ?>" class="btn btn-primary">INICIAR SESION</a>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+        crossorigin="anonymous"></script>
+    </center>
+  </div>
+</body>
+</html>
