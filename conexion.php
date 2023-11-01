f75de0e30c5696ca2ed1bbb14b5f85404cf62799
anthonyapulucm <145887189+anthonyapulucm@users.noreply.github.com>
2023-11-01 16:59:36 -0600

    ACTUALIZACION

--- conexion.php
+++ conexion.php
@@ -1,4 +1,10 @@
 <?php
-//$conexion = new mysqli("pedidos_escolares.sql");
-//$$conexion->set_charset("utf8");
+$conexion = new mysqli("localhost","root","","pedidos_escolares");
+$conexion->set_charset("utf8");
+if (!$conexion) {
+    die("ERROR EN CONEXION");
+} else {
+    echo "CONEXION EXITOSA";
+    $conexion;
+}
 ?>
